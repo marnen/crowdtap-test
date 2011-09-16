8ce11d84c397bf7715522b653c23294551ba4091
Marnen Laibow-Koser <marnen@marnen.org>
2011-09-16 10:17:25 -0400

    Finish setting up controllers for Authlogic.

--- config/routes.rb
+++ config/routes.rb
@@ -55,4 +55,6 @@ CrowdtapTest::Application.routes.draw do
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id(.:format)))'
+  
+  match 'login' => 'user_sessions#create', :as => :login
 end
