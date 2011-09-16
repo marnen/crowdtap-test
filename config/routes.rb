c2d7c20f55c5d55073d29cc44dbfb480e04f14c3
Marnen Laibow-Koser <marnen@marnen.org>
2011-09-16 10:23:36 -0400

    Create simple login page.

--- config/routes.rb
+++ config/routes.rb
@@ -56,5 +56,6 @@ CrowdtapTest::Application.routes.draw do
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id(.:format)))'
   
+  resource :user_session
   match 'login' => 'user_sessions#create', :as => :login
 end
