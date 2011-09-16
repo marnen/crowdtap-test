4e94eaf2aacc0533b337db33addd6a3021a22fb7
Marnen Laibow-Koser <marnen@marnen.org>
2011-09-16 12:58:08 -0400

    Implement Todo#title_with_check.

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,5 +1,18 @@
+# coding: UTF-8
+
 class Todo < ActiveRecord::Base
   belongs_to :user
   acts_as_list :scope => :user
   validates_presence_of :title, :user_id
+  
+  # Sets checked to true and saves the record.
+  def check!
+    update_attribute :checked, true
+  end
+  
+  # Returns the title, preceded by a check if checked? is true.
+  def title_with_check
+    check_string = checked? ? '✓ ' : ''
+    "#{check_string}#{title}"
+  end
 end
