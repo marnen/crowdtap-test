d6feb93f75784871b268facfbbdbde7289cb3fff
Marnen Laibow-Koser <marnen@marnen.org>
2011-09-16 09:18:52 -0400

    rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CrowdtapTest::Application.config.secret_token = 'a2a31ff41998a66d158ba802e0573b3a8b1b4f09dd144da0090d7e9e2804152afa1ebeeb5e49c66d967d5c162b9c3ecdec85704d5a1ac030498ef6e4e0681fca'
